87fcfe5cd57b28fbf50c9b01615aed49fc16543a
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-05-06 15:07:43 -0400

    Definiendo funciones

---
+++ 1. Basicos programacion/3.turtle-funciones.py
@@ -0,0 +1,24 @@
+# modulo de python que sirve para generar programas graficos
+import turtle
+
+def main():
+	window = turtle.Screen()
+	gabriel = turtle.Turtle()
+
+	make_square(gabriel)
+
+	turtle.mainloop()
+
+def make_square(gabriel):
+	length = int(input('Tamano de cuadrado: '))
+
+	for i in range(4):
+		make_line_and_turn(gabriel, length)
+
+def make_line_and_turn(gabriel, length):
+	gabriel.forward(length)
+	gabriel.left(90)
+
+# si python determina que este archivo es el main, empieza desde aqui
+if __name__ == '__main__':
+	main()
