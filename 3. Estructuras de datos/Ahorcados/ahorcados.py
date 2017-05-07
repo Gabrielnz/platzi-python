14501dc0f5c3a670b35b6097b172443413b3b6e4
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-05-07 12:18:41 -0400

    Definiendo interfaz del juego del ahorcado

---
+++ 3. Estructuras de datos/Ahorcados/ahorcados.py
@@ -0,0 +1,107 @@
+
+import random
+
+# Constantes
+IMAGES = ['''
+
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   /    |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   / \  |
+        =========''', '''
+''']
+
+WORDS = [
+    'lavadora',
+    'secadora',
+    'sofa',
+    'gobierno',
+    'diputado',
+    'democracia',
+    'computadora',
+    'teclado'
+]
+
+
+def random_word():
+	return random.choice(WORDS)
+
+
+def display_board(hidden_word, tries):
+	print(IMAGES[tries])
+	print('')
+	print(hidden_word)
+	print('--- * --- * --- * --- * --- * ---')
+
+
+def run():
+	word = random_word()
+	hidden_word = ['-'] * len(word)
+	tries = 0
+
+	while True:
+		display_board(hidden_word, tries)
+		current_letter = str(input('Escoge una letra: '))
+
+
+if __name__ == '__main__':
+	print('B I E N V E N I D O S  A  A H O R C A D O S')
+	run()
