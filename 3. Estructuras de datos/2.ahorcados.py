e085a476d5d77105394cf8e32565032f9954a470
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-05-07 12:55:02 -0400

    Implementando la logica del ahorcado

--- 3. Estructuras de datos/2.ahorcados.py
+++ 3. Estructuras de datos/2.ahorcados.py
@@ -101,6 +101,42 @@ def run():
 		display_board(hidden_word, tries)
 		current_letter = str(input('Escoge una letra: '))
 
+		letter_indexes = []
+		
+		# Almacena los indices de la palabra en donde ocurre la letra presionada
+		for i in range(len(word)):
+			if word[i] == current_letter:
+				letter_indexes.append(i)
+
+		# si la letra presionada no esta en la palabra
+		if len(letter_indexes) == 0:
+			tries += 1
+
+			# si falla con 7 intentos, perdiste.
+			if tries == 7:
+				display_board(hidden_word, tries)
+				print('')
+				print('¡Perdiste! La palabra correcta era: {}'.format(word))
+				break
+
+		# si la letra presionada si esta en la palabra
+		else:
+			# sustituye los simbolos de la palabra escondida
+			# por las occurrencias de la letra encontrada
+			for i in letter_indexes:
+				hidden_word[i] = current_letter
+
+			letter_indexes = []
+
+			# si ya no quedan letras ocultas por descubrir, ganaste.
+			try:
+				hidden_word.index('-')
+			except ValueError:
+				print('')
+				print('¡Felicidades! Ganaste. La palabra es: {}'.format(word))
+				break
+
+
 
 if __name__ == '__main__':
 	print('B I E N V E N I D O S  A  A H O R C A D O S')
